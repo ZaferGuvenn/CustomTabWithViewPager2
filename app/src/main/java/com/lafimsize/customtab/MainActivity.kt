bb54d9723bcb4a3411b9a7bd5987ced1534a1b09
zaferguvenn <zafer.guven58@gmail.com>
2023-02-20 20:04:12 +0300

    added badges

--- app/src/main/java/com/lafimsize/customtab/MainActivity.kt
+++ app/src/main/java/com/lafimsize/customtab/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.lafimsize.customtab
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -26,11 +28,47 @@ class MainActivity : AppCompatActivity() {
 
         }.attach()
 
+        TabLayoutMediator(binding.tLayout2,binding.viewPager){tab,position->
 
-        binding.tLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            when(position){
+
+                1->{
+                    tab.icon=getDrawable(android.R.drawable.ic_dialog_email)
+
+                    val badgeDrawable=tab.orCreateBadge
+                    badgeDrawable.backgroundColor=ContextCompat.getColor(applicationContext,R.color.purple_500)
+                    badgeDrawable.isVisible = true
+                    badgeDrawable.number=100
+                    badgeDrawable.maxCharacterCount=2
+                }
+
+                2->{
+                    tab.icon=getDrawable(android.R.drawable.ic_menu_call)
+
+                    val badgeDrawable=tab.orCreateBadge
+                    badgeDrawable.backgroundColor=ContextCompat.getColor(applicationContext,R.color.teal_200)
+                    badgeDrawable.isVisible = true
+                    badgeDrawable.number=100
+                    badgeDrawable.maxCharacterCount=3
+                }
+
+
+
+            }
+
+
+            tab.text=(position+1).toString()
+
+
+        }.attach()
+
+
+        binding.tLayout2.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 Toast.makeText(this@MainActivity,"selected "+tab?.text,Toast.LENGTH_SHORT).show()
+
+                when(tab?.position){1->tab.badge?.isVisible=false}
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
